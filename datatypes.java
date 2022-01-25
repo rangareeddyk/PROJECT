df627238a2df0a7996a281fc024dd24830966e94
rangareeddyk <98384020+rangareeddyk@users.noreply.github.com>
2022-01-25 21:02:53 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+class e
+{
+static int add(int a, int b)
+{
+return a+b;
+}
+static double add(double a, double b)
+{
+return a*b;
+}
+public static void main(String[] args)
+{
+System.out.println(add(2,5));
+System.out.println(add(2.4,4.5));
+}
+}
